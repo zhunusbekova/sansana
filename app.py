d2bc5f58cf1243baa6bdd6fdfaf4a5879679028f
zhunusbekova <zhunusbekova_a@bi.group>
2025-08-08 21:58:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def index():
     return render_template_string(HTML_TEMPLATE, result=result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render передаст порт сюда
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(host="0.0.0.0", port=port)
